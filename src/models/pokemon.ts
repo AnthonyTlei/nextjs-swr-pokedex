d97f39f8063e893fdf6e3b7582cc4132c0873e0d
Anthony Tleiji <anthonytleiji@gmail.com>
2023-10-31 00:16:03 +0200

    Add Pokemon & PokemonPage models.

---
+++ src/models/pokemon.ts
@@ -0,0 +1,25 @@
+export interface PokemonPage {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: {
+    name: string;
+  }[];
+}
+export interface Pokemon {
+  name: string;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  weight: number;
+  height: number;
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
+}
