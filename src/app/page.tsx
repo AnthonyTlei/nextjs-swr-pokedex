dcab72755de0c2b7df4f27b1d9e940515e9b9f58
Anthony Tleiji <anthonytleiji@gmail.com>
2023-10-31 13:47:10 +0200

    Add Link to Pikachu page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-center mb-4">Gotta cache &apos;em all</h1>
+      <Link href="/pikachu" className="link-light">Pikachu</Link>
     </main>
   )
 }
