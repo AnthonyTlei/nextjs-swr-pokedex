ee3cb45b28cd570ff34bc98399e6113f8724df56
Anthony Tleiji <anthonytleiji@gmail.com>
2023-10-31 15:36:40 +0200

    Add Pokemon list and pagination to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,62 @@
-import Link from "next/link";
+"use client";
 
-export default function Home() {
+import PokemonEntry from "@/components/PokemonEntry";
+import useSWR from "swr";
+import * as PokemonApi from "@/network/pokemon-api";
+import { Button, Col, Row, Spinner } from "react-bootstrap";
+import { usePathname, useRouter } from "next/navigation";
+
+interface HomeProps {
+  searchParams: {
+    page: string;
+  };
+}
+
+export default function Home({ searchParams: { page = "1" } }: HomeProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const pageNumber = parseInt(page);
+  const { data, isLoading } = useSWR(
+    ["getPokemonPage", pageNumber],
+    async () => {
+      return await PokemonApi.getPokemonPage(pageNumber);
+    }
+  );
+
+  if (isLoading) {
+    return <Spinner animation="border" className="d-block m-auto" />;
+  }
+
   return (
     <main>
       <h1 className="text-center mb-4">Gotta cache &apos;em all</h1>
-      <Link href="/pikachu" className="link-light">Pikachu</Link>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-4">
+        {data?.results.map((pokemonEntry) => (
+          <Col key={pokemonEntry.name}>
+            <PokemonEntry name={pokemonEntry.name} />
+          </Col>
+        ))}
+      </Row>
+      <div className="d-flex justify-content-center gap-2 mt-4">
+        {data?.previous && (
+          <Button
+            onClick={() => {
+              router.push(`${pathname}?page=${pageNumber - 1}`);
+            }}
+          >
+            ⬅
+          </Button>
+        )}
+        {data?.next && (
+          <Button
+            onClick={() => {
+              router.push(`${pathname}?page=${pageNumber + 1}`);
+            }}
+          >
+            ➡
+          </Button>
+        )}
+      </div>
     </main>
-  )
+  );
 }
